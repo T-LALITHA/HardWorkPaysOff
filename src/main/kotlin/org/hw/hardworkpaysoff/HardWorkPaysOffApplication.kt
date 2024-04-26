317b849887f979c7f6a6cb9f92d77a31a3de0edb
Lalitha <lally.lalitha.1996@gmail.com>
2024-04-27 00:03:03 +0530

    first commit

---
+++ src/main/kotlin/org/hw/hardworkpaysoff/HardWorkPaysOffApplication.kt
@@ -0,0 +1,11 @@
+package org.hw.hardworkpaysoff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HardWorkPaysOffApplication
+
+fun main(args: Array<String>) {
+	runApplication<HardWorkPaysOffApplication>(*args)
+}
---
+++ src/test/kotlin/org/hw/hardworkpaysoff/HardWorkPaysOffApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hw.hardworkpaysoff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HardWorkPaysOffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
